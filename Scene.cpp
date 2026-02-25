98d39a2ff6466a5b015021209f23c3b0cbf5fca2
Endlia <endlia@qq.com>
2026-02-25 13:45:44 +0800

    done????

--- Scene.cpp
+++ Scene.cpp
@@ -61,4 +61,39 @@ bool Scene::trace(
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
     // TO DO Implement Path Tracing Algorithm here
+    Intersection inter = intersect(ray);
+    if (!inter.happened) {
+        return backgroundColor;
+    }
+    if (inter.m->hasEmission()) {
+        return inter.m->getEmission(); ///return directly for emissive material?
+    }
+
+    Vector3f dir_light = {0., 0., 0.};
+    Intersection lightInter;
+    float pdf_light = 0.0f;
+    sampleLight(lightInter, pdf_light);
+    Vector3f lightDir = (lightInter.coords - inter.coords).normalized(); ///hitpoint->light
+    Ray lightRay(inter.coords + inter.normal * EPSILON, lightDir);
+    Intersection shadowInter = intersect(lightRay);
+    if (shadowInter.happened && (shadowInter.coords - inter.coords).norm() < (lightInter.coords - inter.coords).norm()) {
+        dir_light = {0., 0., 0.};
+    } else {
+        float invDis2 = 1.0f / dotProduct(lightInter.coords - inter.coords, lightInter.coords - inter.coords);
+        dir_light = lightInter.emit * inter.m->eval(-lightDir, -ray.direction, inter.normal) * std::max(0.f, dotProduct(inter.normal, -lightDir))
+         * invDis2 / pdf_light;
+        ///a question: cwise product for color and BRDF?
+    }
+
+    if(get_random_float() > RussianRoulette || depth >= maxDepth) {
+        return dir_light;
+    }
+    Vector3f dir_indirect = {0., 0., 0.};
+    Vector3f wi = inter.m->sample(ray.direction, inter.normal); ///hitpoint->wi
+    Ray indirectRay(inter.coords + inter.normal * EPSILON, wi);
+    if(intersect(indirectRay).happened && !intersect(indirectRay).obj->hasEmit()) {
+        dir_indirect = castRay(indirectRay, depth + 1) * inter.m->eval(-wi, -ray.direction, inter.normal) * std::max(0.f, dotProduct(inter.normal, wi))
+         / inter.m->pdf(wi, -ray.direction, inter.normal) / RussianRoulette;
+    }
+    return dir_light + dir_indirect;
 }
