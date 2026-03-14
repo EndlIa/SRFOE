43f24418acdabb0b185cc0a22c1411014e41c55d
Endlia <endlia@DESKTOP.localdomain>
2026-03-14 16:28:19 +0800

    camera done

--- Camera.hpp
+++ Camera.hpp
@@ -18,18 +18,19 @@ public:
                 0, 0, 0, 1;
     }
 
-    Ray GenerateRay(uint32_t pixelX, uint32_t pixelY, uint32_t width, uint32_t height) const
+    Ray GenerateRay(uint32_t px, uint32_t py, uint32_t width, uint32_t height) const
     {
-        Vector3f dir = GenerateDirection(pixelX, pixelY, width, height);
-        return Ray(CameraM.col(3).to3(), dir);
+        Vector3f localDir = localDirection(px, py, width, height);
+        Vector4f worldDir = CameraM * Vector4f(localDir.x, localDir.y, localDir.z, 0);
+        return Ray(CameraM.col(3).to3(), worldDir.to3());
     }
 
-    Vector3f GenerateDirection(uint32_t pixelX, uint32_t pixelY, uint32_t width, uint32_t height) const
+    Vector3f localDirection(uint32_t px, uint32_t py, uint32_t width, uint32_t height) const
     {
         float scale = std::tan(Deg2Rad(fov * 0.5f));
         float imageAspectRatio = width / static_cast<float>(height);
-        float x = (2 * (pixelX + 0.5f) / static_cast<float>(width) - 1) * imageAspectRatio * scale;
-        float y = (1 - 2 * (pixelY + 0.5f) / static_cast<float>(height)) * scale;
+        float x = (2 * (px + 0.5f) / static_cast<float>(width) - 1) * imageAspectRatio * scale;
+        float y = (1 - 2 * (py + 0.5f) / static_cast<float>(height)) * scale;
         return normalize(Vector3f(-x, y, 1));
     }
 };
